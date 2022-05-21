87663bac6461df7511c0003a709aa52b0aabaa98
Shikhar Amar <81209773+amarshikhar@users.noreply.github.com>
2022-05-21 13:22:18 +0530

    make it better

--- main.py
+++ main.py
@@ -90,9 +90,9 @@ def trainRouteClient():
 
 port = int(os.getenv("PORT",5001))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    #port = 5000
-    ## app.run(debug=True)
-    httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    # host = '0.0.0.0'
+    ## port = 5000
+    app.run(debug=True)
+    # httpd = simple_server.make_server(host, port, app)
+    ## print("Serving on %s %d" % (host, port))
+    # httpd.serve_forever()
